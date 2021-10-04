690d1f82474045a7d4bdfcf97d6103cd5a2950a8
Nathan Berthier <nathanberthier@outlook.fr>
2021-10-04 13:08:35 +0200

    Add users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,12 @@ Post.destroy_all
 City.destroy_all
 User.destroy_all
 puts 'Create user and cities...'
+
 user = User.create!(username: 'Nathan', email: 'nathan.berth69@gmail.com', password: '123456')
+User.create!(username: 'Toto', email: 'toto@gmail.com', password: '123456')
+User.create!(username: 'Tutu', email: 'tutu@gmail.com', password: '123456')
+User.create!(username: 'Titi', email: 'titi@gmail.com', password: '123456')
+
 City.create!(name: 'Bourg en Bresse', short_name: 'bourg', subscription_end: (DateTime.current + 3), lastupload_log: 'Prout', lastupload_date: DateTime.current, user: user)
 City.create!(name: 'Lyon', short_name: 'lyon', subscription_end: (DateTime.current - 2), lastupload_log: 'Prout', lastupload_date: DateTime.current, user: user)
 City.create!(name: 'Paris', short_name: 'paris', subscription_end: (DateTime.current + 10), lastupload_log: 'Prout', lastupload_date: DateTime.current, user: user)
